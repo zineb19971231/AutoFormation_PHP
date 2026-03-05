55ef583afce83c58564f68040b75468e16137172
zineb19971231 <zinebakarras@gmail.com>
2026-03-05 21:04:09 +0000

    update_file_algorithme

---
+++ challenge_3.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$friends= array("ahmed"=>"100","mohamed"=>"200","sayed"=>"100","eman"=>"70");
+foreach( $friends as $key => $value){
+     array_sum($friends)  ;
+}
+
+echo  "the total is " , array_sum($friends) , " MAD";
+echo "<br> the ones how owe more than 100 MAD are : " ;
+foreach( $friends as $key => $value)
+{
+    
+}
+
+?>
