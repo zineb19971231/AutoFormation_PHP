ad8c85342f6272b7cd18a64e8af39295bb12cfe6
zineb19971231 <zinebakarras@gmail.com>
2026-03-06 10:34:45 +0000

    modifications file challenge 3

--- challenge_3.php
+++ challenge_3.php
@@ -4,13 +4,12 @@
 $friends= array("ahmed"=>"100","mohamed"=>"200","sayed"=>"100","eman"=>"70");
 foreach( $friends as $key => $value){
      array_sum($friends)  ;
+     if ($value > 100) {
+         echo " the ones who owe more than 100 MAD are : <br>" . $key . " : " . $value . "<br>";
+     }
 }
+echo  "the total is " , array_sum($friends) , " MAD ";
 
-echo  "the total is " , array_sum($friends) , " MAD";
-echo "<br> the ones how owe more than 100 MAD are : " ;
-foreach( $friends as $key => $value)
-{
-    
-}
+
 
 ?>
