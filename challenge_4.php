47c13279fe1fa005888af731beaf823e9b1c93d0
zineb19971231 <zinebakarras@gmail.com>
2026-03-06 11:23:51 +0000

    Add file challenge43

---
+++ challenge_4.php
@@ -0,0 +1,56 @@
+<?php 
+
+$counter =0;
+while ($counter < 10){
+    echo "$counter <br>";
+    $counter++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
